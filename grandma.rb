6af0577821bc41d58254bb201f37ce4f6e52fd38
Kasey Arnold <kaseyarnold15@gmail.com>
2020-01-23 16:54:35 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,7 @@ def speak_to_grandma(phrase)
     if phrase == "I LOVE YOU GRANDMA!"
       "I LOVE YOU TOO PUMPKIN!"
   
-    elsif phrase == phrase.downcase
+    elsif phrase == phrase.upcase
       "NO, NOT SINCE 1938!"
   
     else 
